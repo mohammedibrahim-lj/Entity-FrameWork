e34bcd165b4e4823783324b98bab3eec8a9210d9
mohammedibrahim-lj <mohammedibrahim.lj@gmail.com>
2016-01-09 11:12:08 +0530

    Primary Key

--- EntityFrameWorkDemos/Program.cs
+++ EntityFrameWorkDemos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,22 @@ namespace EntityFrameWorkDemos
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            DBCreate db = new DBCreate();
+            db.StuTab.Add(new StudentTable { StudentName = "ibrahim", StudentClass = "MCA", StudentAge = "26" });
+            db.StuTab.Add(new StudentTable { StudentName = "ibu", StudentClass = "MCA", StudentAge = "26" });
+            db.StuTab.Add(new StudentTable { StudentName = "MMM", StudentClass = "MCA", StudentAge = "26" });
+            db.StuTab.Add(new StudentTable { StudentName = "ddd", StudentClass = "MCA", StudentAge = "26" });
+            db.StuTab.Add(new StudentTable { StudentName = "EEE", StudentClass = "MCA", StudentAge = "26" });
+            db.SaveChanges();
+            var datas = db.StuTab.ToList();
+            foreach(var items in datas)
+            {
+                Console.WriteLine(items.StudentName.ToString());
+            }
+            Console.ReadKey();
         }
     }
     class StudentTable
@@ -22,6 +37,17 @@ namespace EntityFrameWorkDemos
     }
     class DBCreate:DbContext
     {
+        public DBCreate()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<DBCreate>());
+        }
+       
         public DbSet<StudentTable> StuTab { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentTable>().HasKey(t => t.StudentID);
+            base.OnModelCreating(modelBuilder);
+        }
     }
+    
 }
